dc77f2b80f42ab388cb3f150c8b9508480529579
Peter Broadbery <p.broadbery@gmail.com>
2011-03-20 21:53:07 +0000

    Added parameters to test runner

--- aldor/aldor/src/test/testall.c
+++ aldor/aldor/src/test/testall.c
@@ -1,10 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "testall.h"
+
+static int theArgc;
+static char **theArgv;
 
 int 
 main(int argc, char *argv[])
 {
-	fnameTest();
+	theArgc = argc;
+	theArgv = argv;
+
+	if (testShouldRun("fname")) fnameTest();
+
 	testShowSummary();
 	
 	return testAllPassed() ? 0 : 1;
+}
+
+int
+testShouldRun(char *name)
+{
+	int i;
+
+	if (theArgc == 1) {
+		return 1;
+	}
+	for (i=1; i<theArgc; i++) {
+		if (strcmp(theArgv[i], name) == 0) {
+			return 1;
+		}
+	}
+	return 0;
 }
