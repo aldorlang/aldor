4d8430cb693f058708020ef6aaa5bc1bfff008e6
Peter Broadbery <p.broadbery@gmail.com>
2012-01-07 23:40:05 +0000

    turns out that ostream printf doesn't do what we want it to do.  Should be feature for feature with standard printf

--- aldor/aldor/src/test/test_printf.c
+++ aldor/aldor/src/test/test_printf.c
@@ -3,10 +3,12 @@
 #include "testlib.h"
 
 local void testPrintf1();
+local void testPrintf2();
 
 void printfTest()
 {
 	TEST(testPrintf1);
+	TEST(testPrintf2);
 }
 
 char arr[100];
@@ -42,4 +44,31 @@ testPrintf1()
 	
 	testStringEqual("content", arr2, arr);
 	testIntEqual("retval", cc, strlen(arr));
+}
+
+local void checkPrintfCase(int w, const char *txt);
+local void
+testPrintf2()
+{
+	checkPrintfCase(0, "");
+	checkPrintfCase(1, "");
+	checkPrintfCase(2, "");
+	checkPrintfCase(0, "x");
+	checkPrintfCase(1, "x");
+	checkPrintfCase(2, "x");
+	checkPrintfCase(1, "xy");
+	checkPrintfCase(5, "xy");
+
+}
+
+local void
+checkPrintfCase(int w, const char *txt)
+{
+	String name = strPrintf("%d-[%s]", w, txt);
+	char buf[20];
+	sprintf(buf, "%*s", w, txt);
+	String s2 = strPrintf("%*s", w, txt);
+
+	testStringEqual(name, buf, s2);
+	strFree(name);
 }
