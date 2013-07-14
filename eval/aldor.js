72d33422c5eba9a9f1b710dd218ef4932e1b9f78
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-07-14 03:40:53 +0200

    add eval-aldor pages

---
+++ eval/aldor.js
@@ -0,0 +1,80 @@
+jQuery(function ($) {
+   var saved = {};// = $.parseJSON(document.cookie);
+
+   function store () {
+      document.cookie = JSON.stringify(saved);
+   }
+
+   function add_snippet (name, code) {
+      var li = $('<li style="cursor: pointer"/>');
+      $('#snippets').append(
+         li.append(
+            $('<a/>').text(name).click(function (event) {
+               $('#code').val(code);
+            })
+         ).append(
+            $('<img src="delete.png" style="height: 1em; width: 1em"/>').click(function (event) {
+               li.remove();
+               delete saved[name];
+               store();
+            })
+         )
+      );
+   }
+
+   $.each(saved, add_snippet);
+   
+   var spinner = $('<p class="spinner"><img src="loading.gif"/></p>');
+
+   $('#run').click(function (event) {
+      var response = $('#response');
+      response.addClass("spinner");
+      response.html(spinner);
+
+      $('#run').attr("disabled", "disabled");
+
+      $.post('http://xinutec.org:9090/', {
+         code: $('#code').val(),
+         line: $('#line').val()
+      }, function (data) {
+         response.removeClass("spinner");
+         response.html(data);
+         $('#run').removeAttr("disabled");
+      });
+   });
+
+   $('#form').submit(function (event) {
+      event.preventDefault();
+      if (!$('#run').attr('disabled')) {
+         $('#run').click();
+      }
+   });
+
+   $('#save').click(function (event) {
+      var defaultName = "Snippet #" + (Object.keys(saved).length + 1);
+      var name = prompt("Enter a name for your snippet", defaultName);
+      if (name == null) {
+         return;
+      }
+
+      var code = $('#code').val();
+      saved[name] = code;
+      add_snippet(name, code);
+      store();
+   });
+
+   function example (name) {
+      $.get('examples/' + name + '.as', function (data) {
+         $('#code').val(data);
+      });
+   }
+
+   $('#examples a').click(function (event) {
+      example($(event.target).text());
+   });
+
+   // load introductory example
+   example("intro");
+
+   $('#line').focus();
+})
---
+++ eval/examples/intro.as
@@ -0,0 +1,7 @@
+-- Welcome to the Online Aldor Interpreter.
+
+-- The code you type here will be compiled to [t.ao] and loaded into the Interpreter.
+-- You can send a single command to the Aldor Read-Eval-Print-Loop (REPL), and the
+-- results are printed below. If you press [Save], a link will appear on the left,
+-- which you can click to restore the code. These saved snippets are stored in a
+-- browser cookie. Now, press [Run].
