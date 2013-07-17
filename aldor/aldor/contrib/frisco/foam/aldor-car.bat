cec5859ee554e7048b21a4f21dcc1afcb2b92b82
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-07-17 10:50:38 +0200

    removed old build scripts

--- aldor/aldor/contrib/frisco/foam/aldor-car.bat
+++
@@ -1,8 +0,0 @@
-rem #!/bin/bsh
-if not defined GmpDir (
-	echo "no GmpDir defined -- the linker will fail unless the GMP library is in a well-known place"
-	dir="" 
-) else (
-	dir="-Y $GmpDir"
-)
-$ALDORROOT\bin\aldor $dir -Cruntime=foam-car,BigInt,gmp,stdc++,cmm $*
