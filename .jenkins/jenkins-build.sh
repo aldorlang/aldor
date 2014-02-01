ef8ca361d1b71685e091991a0569b5d0e067671b
Peter Broadbery <p.broadbery@gmail.com>
2014-02-01 18:31:16 +0000

    Add freebsd clang and gcc targets

--- .jenkins/jenkins-build.sh
+++ .jenkins/jenkins-build.sh
@@ -7,16 +7,32 @@ case $JOB_NAME in
     aldor-gcc-m32)
 	configure_options='CFLAGS=-m32 LDFLAGS=-m32 UNICLFLAGS=-Wsys=linux-x32'
 	build_options=UNICLFLAGS=-Wsys=linux-x32
+	make=make
 	;;
     aldor-clang)
 	configure_options='--with-boehm-gc CC=clang UNICLFLAGS=-Wsys=linuxclang'
 	build_options=UNICLFLAGS=-Wsys=linuxclang
+	make=make
+	;;
+    freebsd-clang)
+	configure_options='--with-boehm-gc CC=clang UNICLFLAGS=-Wsys=linuxclang'
+	build_options=UNICLFLAGS=-Wsys=linuxclang
+	make=gmake
+	;;
+    freebsd-gcc)
+	configure_options='--with-boehm-gc CC=gcc UNICLFLAGS=-Wsys=freebsd-gcc'
+	build_options=UNICLFLAGS=-Wsys=freebsd-gcc
+	make=gmake
 	;;
     *)
 	echo "Unknown job name"
 	;;
 esac
 
+if [ "$make" = "" ];
+then
+    exit 1
+fi
 
 thisdir=$(pwd)
 cd aldor
@@ -26,6 +42,6 @@ rm -rf build
 mkdir build
 cd $thisdir/build
 $thisdir/aldor/configure --prefix=$thisdir/opt $configure_options
-make -j $build_options && make check $build_options -j && make install $build_options -j && make -j installcheck
+$make -j $build_options && make check $build_options -j && gmake install $build_options -j && $make -j installcheck
 
 
