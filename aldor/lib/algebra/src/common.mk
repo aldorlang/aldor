a7d91cecc190824ec0ffc54b29bee934ba6e3304
Peter Broadbery <p.broadbery@gmail.com>
2014-02-15 20:15:46 +0000

    lib/algebra: compile with -Q2; -Q8 just yields unfeasibly large methods (and the important functions remain uninlined).

--- aldor/lib/algebra/src/common.mk
+++ aldor/lib/algebra/src/common.mk
@@ -5,7 +5,7 @@ libraryname	:= algebra
 librarydeps     := aldor
 
 #AXLCDB		:= -W check -Csmax=0 -Zdb -Qno-cc
-AXLFLAGS	:= -Z db -Q8 $(AXLCDB)
+AXLFLAGS	:= -Z db -Q2 $(AXLCDB)
 AXLFLAGS	+= -Y $(aldorlibdir) -I $(aldorincdir) -laldor
 
 include $(top_srcdir)/lib/buildlib.mk
