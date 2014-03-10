c01c1364b0226ee8748d4636cd28a9702c5eebcc
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-03-10 21:25:20 +0100

    define the *_SOURCE variables as 1
    System headers do so, so we do the same.

--- aldor/aldor/src/opsys.c
+++ aldor/aldor/src/opsys.c
@@ -7,9 +7,9 @@
  ****************************************************************************/
 
 
-#define _ALL_SOURCE   /* For RS/6000 - should come before cport.h include. */
-#define _POSIX_SOURCE /* For Linux/BSD. */
-#define _BSD_SOURCE   /* sbrk */
+#define _ALL_SOURCE 1   /* For RS/6000 - should come before cport.h include. */
+#define _POSIX_SOURCE 1 /* For Linux/BSD. */
+#define _BSD_SOURCE 1  /* sbrk */
 
 #include "cport.h"
 #include "editlevels.h"
