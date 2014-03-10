357e06d04c39d081176c3c491f1e986dff394080
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-03-10 21:32:03 +0100

    formatting

--- aldor/aldor/src/util.c
+++ aldor/aldor/src/util.c
@@ -472,12 +472,13 @@ hashCombinePair(int i1, int i2)
 
 	if (sizeof(long) >= 8) {
 		IF_LongOver32Bits(
-				long zz = (zzh << 32) + zzl;
-				long h1 = i1 & ((1L<<32)-1);
-				long h2 = i2 & ((1L<<32)-1);
+			long zz = (zzh << 32) + zzl;
+			long h1 = i1 & ((1L<<32)-1);
+			long h2 = i2 & ((1L<<32)-1);
 
-				int tmp = (int)(((z1*h1 + z2*h2) * zz) >> 32);
-				return tmp & 0x3FFFFFFF;)
+			int tmp = (int)(((z1*h1 + z2*h2) * zz) >> 32);
+			return tmp & 0x3FFFFFFF;
+		)
 	}
 	else if (sizeof(long) == 4 && sizeof(unsigned int) == 4) {
 		BInt bi1 = bintNew(i1);
