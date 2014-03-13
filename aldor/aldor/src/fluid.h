e236f24023eb0d46718d8dd593f49afcd1c1080d
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-03-13 15:35:12 +0100

    add fluidSave_ before fluidSave'd variables
    Before, it added __, which is not compliant:
    
    cstd 7.1.3 Reserved identifiers
    
    - All identifiers that begin with an underscore and either an uppercase letter
      or another underscore are always reserved for any use.

--- aldor/aldor/src/fluid.h
+++ aldor/aldor/src/fluid.h
@@ -99,7 +99,7 @@ extern void		 fluidUnwind	(int toCell, Bool isLongJmp);
 /*
  * Introduction of individual fluids.
  */
-#define fluidSave(var)  __##var
+#define fluidSave(var)  fluidSave_##var
 
 #define fluid(V)	fluidSave(V) = ( \
 	fluidStack = (fluidLevel==fluidLimit) ? fluidGrow() : fluidStack,    \
