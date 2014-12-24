659addd29167159dc08d139dcaf6036801efa427
Peter Broadbery <p.broadbery@gmail.com>
2014-12-24 12:47:56 +0000

    formatters: %oBool now shows true or false. Of Bool[23123412] if you write buggy code.

--- aldor/aldor/src/formatters.c
+++ aldor/aldor/src/formatters.c
@@ -49,9 +49,13 @@ local int errorSetFormatter(OStream stream, Pointer p);
 local int javaCodeFormatter(OStream stream, Pointer p);
 
 
+local int boolFormatter(OStream ostream, int p);
+
 void
 fmttsInit()
 {
+	fmtRegisterI("Bool", boolFormatter);
+
 	fmtRegister("TForm", tfFormatter);
 	fmtRegister("TFormList", tfListFormatter);
 
@@ -305,4 +309,18 @@ stringListFormatter(OStream ostream, Pointer p)
 {
 	StringList list = (StringList) p;
 	return listFormat(String)(ostream, "String", list);
+}
+
+local int
+boolFormatter(OStream ostream, int p)
+{
+	Bool flg = p;
+
+	if (flg < 0 || flg > 1)	{
+		return ostreamPrintf(ostream, "Bool[%d]", flg);
+	}
+	else {
+		return ostreamPrintf(ostream, "%s", flg ? "true": "false");
+	}
+
 }
