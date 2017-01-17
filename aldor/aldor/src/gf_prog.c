fc96e10951ae4f5fd654a4c5bc9367ed9d04e42d
Peter Broadbery <p.broadbery@gmail.com>
2017-01-17 23:07:47 +0000

    genfoam: deal with try/catch returning no value

--- aldor/aldor/src/gf_prog.c
+++ aldor/aldor/src/gf_prog.c
@@ -14,6 +14,7 @@
 #include "syme.h"
 #include "strops.h"
 #include "fbox.h"
+#include "tform.h"
 
 /*****************************************************************************
  *
@@ -55,9 +56,14 @@ gen0BuildFunction(ProgType pt, String name, AbSyn expr)
 
 	saved = gen0ProgSaveState(pt);
 
-	ret = genFoamVal(expr);
-	if (ret) gen0AddStmt(foamNewReturn(ret), expr);
-
+	if (tfIsNone(gen0AbContextType(expr))) {
+		genFoamStmt(expr);
+		gen0AddStmt(foamNewReturn(foamNew(FOAM_Values, (Length) 0)), expr);
+	}
+	else {
+		ret = genFoamValAs(gen0AbContextType(expr), expr);
+		if (ret) gen0AddStmt(foamNewReturn(ret), expr);
+	}
 	gen0ProgPushFormat(emptyFormatSlot);
 	gen0IssueDCache();
 
