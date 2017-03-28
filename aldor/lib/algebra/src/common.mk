0a2de96315935a476976a4827ffdc3f1ef26595e
Peter Broadbery <p.broadbery@gmail.com>
2017-03-28 23:22:33 +0100

    Build: Copy source library to $prefix/share/lib - Will be handy for UI stuff

--- aldor/lib/algebra/src/common.mk
+++ aldor/lib/algebra/src/common.mk
@@ -1,5 +1,6 @@
 aldorincdir	:= $(top_srcdir)/lib/aldor/include
 aldorlibdir	:= $(top_builddir)/lib/aldor/src
+abs_libdir	:= $(abs_top_builddir)/lib/algebra/src
 
 libraryname	:= algebra
 librarydeps     := aldor
