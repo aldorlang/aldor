3d0a31fd2157fc7d9064a3c5f227d4f7a1a5f5e7
Peter Broadbery <p.broadbery@gmail.com>
2017-09-09 16:13:30 +0100

    lib/java: Add Java object for use by Foam.

--- aldor/aldor/lib/java/src/foamj/FoamJ.java
+++ aldor/aldor/lib/java/src/foamj/FoamJ.java
@@ -34,6 +34,35 @@ public class FoamJ {
     }
 
     /**
+     * JavaObj type - rely on casting to retract to base type
+     */
+    static public class JavaObj<T> extends AbstractValue implements Value, Word {
+        private T obj;
+
+        JavaObj(T obj) {
+            this.obj = obj;
+        }
+
+        public T toJavaObj() {
+            return obj;
+        }
+
+        @Override
+        public Value toValue() {
+            return this;
+        }
+
+        public Word asWord() {
+            return this;
+        }
+
+        public String toString() {
+            return obj.toString();
+        }
+
+    }
+
+    /**
      * a.
      * T1 = (Add (Cast SInt x) 1)
      * -->
