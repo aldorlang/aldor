ccadc606c73493a9f45cea7093cfea78339f6cbd
Peter Broadbery <p.broadbery@gmail.com>
2019-06-03 21:52:14 +0100

    tposs.c: Change iterator to be function based, not macro.

--- aldor/aldor/src/tposs.h
+++ aldor/aldor/src/tposs.h
@@ -105,9 +105,11 @@ typedef struct {
 	TFormList	possl;
 } TPossIterator;
 
+extern TForm tpossELT_(TPossIterator *ip);
+
 #define tpossITER(ip,p)	((ip).possl = (p ? (p)->possl : NULL))
 #define tpossMORE(ip)   ((ip).possl)
 #define tpossSTEP(ip)	((ip).possl = cdr((ip).possl))
-#define tpossELT(ip)	car((ip).possl)
+#define tpossELT(ip)    tpossELT_(&ip)
 
 #endif /* !_TPOSS_H_ */
