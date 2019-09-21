04de2f23cc9e5e632b3ce8ebd70b8c89e8652208
Peter Broadbery <p.broadbery@gmail.com>
2019-09-21 19:33:23 +0100

    makefiles: Generate -sources jar files

--- aldor/lib/buildlib.mk
+++ aldor/lib/buildlib.mk
@@ -31,7 +31,7 @@ am__v_$(1)_  = $$(am__v_$(1)_$$(AM_DEFAULT_VERBOSITY))
 am__v_$(1)_0 = @
 endef
 
-STEPS := ALDOR AO2C AO2FM AR DEP FOAMJ JAR JAR JAVAC
+STEPS := ALDOR AO2C AO2FM AR DEP FOAMJ JAR JAR JAVAC SRCJAR
 QUIET_STEPS := ALDORTEST ALDORTESTJ
 
 $(foreach rule,$(STEPS),$(eval $(call am_silent_template,$(rule))))
@@ -199,14 +199,26 @@ $(libraryname).jar: $(patsubst %,aldorcode/%.class, $(_javalibrary)) $(top_srcdi
 	rm -f $@;	\
 	rm -rf jar;	\
 	mkdir jar;	\
-	jar cf $@ $(patsubst %,aldorcode/%*.class, $(_javalibrary))
+	jar cf $@ $(patsubst %,aldorcode/%*.class, $(_javalibrary));  \
 	for i in $(foreach i, $(SUBDIRS), $i/$(libraryname).jar); do \
 		(cd jar; jar xf ../$$i);				\
 		jar uf ../$@ -C jar .; done;				\
 	rm -rf jar
 
-all: $(libraryname).jar				\
+$(libraryname)-sources.jar: $(patsubst %,aldorcode/%.class, $(_javalibrary)) $(top_srcdir)/lib/buildlib.mk
+	$(AM_V_SRCJAR) \
+	rm -f $@;	\
+	rm -rf sources-jar;	\
+	mkdir sources-jar;	\
+	jar cf $@ $(patsubst %,aldorcode/%*.java, $(_javalibrary)); \
+	for i in $(foreach i, $(SUBDIRS), $i/$(libraryname)-sources.jar); do \
+		(cd jar; jar xf ../$$i);				\
+		jar uf ../$@ -C sources-jar .; done;				\
+	rm -rf jar
+
+all: $(libraryname).jar	$(libraryname)-sources.jar \
 	$(patsubst %,aldorcode/%.class,$(_javalibrary))
+
 endif
 endif
 
