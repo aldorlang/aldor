bb6eda043611d011c34587086128c14ad8fefb45
Peter Broadbery <p.broadbery@gmail.com>
2024-09-14 14:54:36 +0100

    syme0.h: New header to avoid cycles

---
+++ aldor/aldor/src/syme0.h
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ *
+ * syme0.h: syme stuff that would otherwise cause include cycles
+ *
+ * Copyright (c) 1990-2007 Aldor Software Organization Ltd (Aldor.org).
+ *
+ ****************************************************************************/
+
+#ifndef SYME0_H
+#define SYME0_H
+
+# define SYME_NUMBER_UNASSIGNED (0x7FFF)
+# define SYME_DEPTH_UNUSED	(0x7FFF)
+
+#endif
